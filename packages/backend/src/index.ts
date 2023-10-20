38b0289f3e0a078cc5d1cfe696607ba76a1e358a
Michael Jakl <michael.jakl@semanticlabs.at>
2023-10-20 16:47:41 +0200

    feat(backend): wire the Kubernetes backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -26,6 +26,7 @@ import Router from 'express-promise-router';
 import app from './plugins/app';
 import auth from './plugins/auth';
 import catalog from './plugins/catalog';
+import kubernetes from './plugins/dynatrace-kubernetes';
 import proxy from './plugins/proxy';
 import scaffolder from './plugins/scaffolder';
 import search from './plugins/search';
@@ -85,6 +86,9 @@ async function main() {
   const techdocsEnv = useHotMemoize(module, () => createEnv('techdocs'));
   const searchEnv = useHotMemoize(module, () => createEnv('search'));
   const appEnv = useHotMemoize(module, () => createEnv('app'));
+  const dynatraceKubernetesEnv = useHotMemoize(module, () =>
+    createEnv('dynatrace-kubernetes'),
+  );
 
   const apiRouter = Router();
   apiRouter.use('/catalog', await catalog(catalogEnv));
@@ -93,6 +97,10 @@ async function main() {
   apiRouter.use('/techdocs', await techdocs(techdocsEnv));
   apiRouter.use('/proxy', await proxy(proxyEnv));
   apiRouter.use('/search', await search(searchEnv));
+  apiRouter.use(
+    '/dynatrace-kubernetes',
+    await kubernetes(dynatraceKubernetesEnv),
+  );
 
   // Add backends ABOVE this line; this 404 handler is the catch-all fallback
   apiRouter.use(notFoundHandler());
---
+++ packages/backend/src/plugins/dynatrace-kubernetes.ts
@@ -0,0 +1,11 @@
+import { createRouter } from 'backstage-plugin-kubernetes-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
+  return await createRouter({
+    logger: env.logger,
+  });
+}
