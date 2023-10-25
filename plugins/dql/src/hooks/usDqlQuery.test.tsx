d74df3387775d0023ebe453150216d99890db439
Michael Jakl <michael.jakl@semanticlabs.at>
2023-10-25 19:19:03 +0200

    test(backstage-plugin-dql): cover the useDqlQuery hook

--- plugins/dql/src/hooks/usDqlQuery.test.tsx
+++ plugins/dql/src/hooks/usDqlQuery.test.tsx
@@ -1,41 +1,63 @@
-import { DqlQueryApi, dqlQueryApiRef } from '../api';
+import { dqlQueryApiRef } from '../api';
 import { useDqlQuery } from './useDqlQuery';
 import { TestApiProvider } from '@backstage/test-utils';
 import { TabularData } from '@dynatrace/backstage-plugin-dql-common';
 import { renderHook } from '@testing-library/react-hooks';
 import React, { ReactNode } from 'react';
 
-class MockDqlQueryApi implements DqlQueryApi {
-  async getData(): Promise<TabularData> {
-    return [];
-  }
-}
+const MockDqlQueryApi = {
+  getData: jest.fn().mockResolvedValue([] as TabularData),
+};
 
 const wrapper = ({ children }: { children: ReactNode }) => (
-  <TestApiProvider apis={[[dqlQueryApiRef, new MockDqlQueryApi()]]}>
+  <TestApiProvider apis={[[dqlQueryApiRef, MockDqlQueryApi]]}>
     {children}
   </TestApiProvider>
 );
 
 describe('usDqlQuery', () => {
-  it('should return the result of the query', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should delegate to dqlQueryApi and return the result of the query', async () => {
     const { result, waitForNextUpdate } = renderHook(
       () => useDqlQuery('namespace', 'queryName', 'component'),
       { wrapper },
     );
 
-    expect(result.current).toEqual({
-      error: undefined,
-      loading: true,
-      value: undefined,
-    });
+    await waitForNextUpdate();
+
+    expect(MockDqlQueryApi.getData).toHaveBeenCalledWith(
+      'namespace',
+      'queryName',
+      'component',
+    );
+    expect(result.current.value).toEqual([]);
+  });
+
+  it('should return loading true while the query is running', async () => {
+    const { result, waitForNextUpdate } = renderHook(
+      () => useDqlQuery('namespace', 'queryName', 'component'),
+      { wrapper },
+    );
+
+    expect(result.current.loading).toBeTruthy();
+
+    await waitForNextUpdate();
+  });
+
+  it('should return error if the query fails', async () => {
+    const error = new Error('test');
+    MockDqlQueryApi.getData.mockRejectedValue(error);
+
+    const { result, waitForNextUpdate } = renderHook(
+      () => useDqlQuery('namespace', 'queryName', 'component'),
+      { wrapper },
+    );
 
     await waitForNextUpdate();
 
-    expect(result.current).toEqual({
-      error: undefined,
-      loading: false,
-      value: [],
-    });
+    expect(result.current.error).toEqual(error);
   });
 });
