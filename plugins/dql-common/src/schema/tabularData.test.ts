904888b7634f1240c31caba7e2b5f4597ec67a85
Michael Jakl <michael.jakl@semanticlabs.at>
2023-10-25 14:18:22 +0200

    test(backstage-plugin-dql-common): test with multiple small objects

--- plugins/dql-common/src/schema/tabularData.test.ts
+++ plugins/dql-common/src/schema/tabularData.test.ts
@@ -1,20 +1,33 @@
 import { TabularDataFactory } from './tabularData';
 
-const VALID_OBJECT = [
-  {
-    name: 'foo',
-    namespace: 'bar',
-    extra: { type: 'link', text: 'example.com', url: 'https://example.com' },
-  },
-  {
-    name: 'baz',
-    namespace: 'qux',
-    extra: { type: 'link', text: 'google.com', url: 'https://google.com' },
-  },
+const VALID_OBJECTS = [
+  [], // Empty array
+  [
+    // Standard case
+    {
+      name: 'foo',
+      namespace: 'bar',
+    },
+    {
+      name: 'baz',
+      namespace: 'qux',
+    },
+  ],
+  [
+    // with Link
+    {
+      name: 'foo',
+      namespace: 'bar',
+      extra: { type: 'link', text: 'example.com', url: 'https://example.com' },
+    },
+  ],
 ];
 const INVALID_OBJECTS = [
   [
-    ...VALID_OBJECT,
+    {
+      name: 'some-name',
+      extra: 'foo',
+    },
     {
       extra: 'foo', // different structure (missing columns)
     },
@@ -30,11 +43,13 @@ const INVALID_OBJECTS = [
 describe('TabularDataFactory', () => {
   describe('fromString', () => {
     it('parses valid input', () => {
-      const input = JSON.stringify(VALID_OBJECT);
+      VALID_OBJECTS.forEach(validInput => {
+        const input = JSON.stringify(validInput);
 
-      const deployment = TabularDataFactory.fromString(input);
+        const deployment = TabularDataFactory.fromString(input);
 
-      expect(deployment).toEqual(VALID_OBJECT);
+        expect(deployment).toEqual(validInput);
+      });
     });
 
     it('throws on invalid inputs', () => {
@@ -48,11 +63,13 @@ describe('TabularDataFactory', () => {
 
   describe('fromObject', () => {
     it('parses valid input', () => {
-      const input = VALID_OBJECT;
+      VALID_OBJECTS.forEach(validInput => {
+        const input = validInput;
 
-      const deployment = TabularDataFactory.fromObject(input);
+        const deployment = TabularDataFactory.fromObject(input);
 
-      expect(deployment).toEqual(VALID_OBJECT);
+        expect(deployment).toEqual(validInput);
+      });
     });
 
     it('throws on invalid inputs', () => {
