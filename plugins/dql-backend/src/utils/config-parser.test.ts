e466301a5f915c96383f3f3a700fa14af3cd5d23
Michael Jakl <michael.jakl@semanticlabs.at>
2023-11-13 17:35:46 +0100

    test(backstage-plugin-dql-backend): add tests for the config parser

---
+++ plugins/dql-backend/src/utils/config-parser.test.ts
@@ -0,0 +1,48 @@
+import { parseCustomQueries, parseEnvironments } from './config-parser';
+import { MockConfigApi } from '@backstage/test-utils';
+
+const TEST_ENVIRONMENT = {
+  name: 'test',
+  url: 'https://test.dynatrace.com',
+  tokenUrl: 'https://test.dynatrace.com',
+  clientId: 'test',
+  clientSecret: 'test',
+  accountUrn: 'test',
+};
+
+describe('config-parser', () => {
+  describe('parseEnvironments', () => {
+    it('should return a list of DynatraceApis with the corresponding config', () => {
+      const config = new MockConfigApi({
+        dynatrace: { environments: [TEST_ENVIRONMENT] },
+      });
+      const result = parseEnvironments(config);
+
+      expect(result).toHaveLength(1);
+      const api = result[0];
+      expect(api.environmenName).toEqual(TEST_ENVIRONMENT.name);
+      expect(api.environmentUrl).toEqual(TEST_ENVIRONMENT.url);
+    });
+  });
+
+  describe('parseCustomQueries', () => {
+    it('should return a list of custom queries', () => {
+      const queryId = 'test';
+      const query = 'fetch...';
+
+      const config = new MockConfigApi({
+        dynatrace: {
+          queries: [
+            {
+              id: queryId,
+              query: query,
+            },
+          ],
+        },
+      });
+      const result = parseCustomQueries(config);
+
+      expect(result).toEqual({ [queryId]: query });
+    });
+  });
+});
