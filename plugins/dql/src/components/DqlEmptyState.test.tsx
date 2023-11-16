295bdd5e37f9253aa710c9a542a731f6e1716e72
Michael Jakl <michael.jakl@semanticlabs.at>
2023-11-16 12:12:29 +0100

    feat(backstage-plugin-dql): add DqlEmptyState component

---
+++ plugins/dql/src/components/DqlEmptyState.test.tsx
@@ -0,0 +1,42 @@
+import { DqlEmptyState } from './DqlEmptyState';
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const prepareComponent = ({
+  componentName = 'example',
+  componentNamespace = 'default',
+  queryName = 'query',
+  queryNamespace = 'dynatrace',
+}: {
+  componentName?: string;
+  componentNamespace?: string;
+  queryName?: string;
+  queryNamespace?: string;
+}) => {
+  return render(
+    <DqlEmptyState
+      componentName={componentName}
+      componentNamespace={componentNamespace}
+      queryName={queryName}
+      queryNamespace={queryNamespace}
+    />,
+  );
+};
+
+describe('DqlEmptyState', () => {
+  it('should include the component and query references in the explanation', () => {
+    const componentName = 'example';
+    const componentNamespace = 'default';
+    const queryName = 'query-id-1';
+    const queryNamespace = 'dynatrace';
+    const rendered = prepareComponent({
+      componentName,
+      componentNamespace,
+      queryName,
+      queryNamespace,
+    });
+
+    expect(rendered.getByText(/default\/example/)).toBeInTheDocument();
+    expect(rendered.getByText(/dynatrace.query-id-1/)).toBeInTheDocument();
+  });
+});
