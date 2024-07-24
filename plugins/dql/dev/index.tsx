9d585c8fe65068df722ccefdf65f242fea599ef6
blasget <tobias.blasge@speed.at>
2024-07-24 09:14:07 +0200

    fix: tsc and tests

--- plugins/dql/dev/index.tsx
+++ plugins/dql/dev/index.tsx
@@ -19,6 +19,7 @@ import {
   dqlQueryPlugin,
 } from '../src';
 import { DqlQueryApi, dqlQueryApiRef } from '../src/api';
+import { NotebookQueryData } from '../src/api/types';
 import { exampleData } from './data';
 import { Entity } from '@backstage/catalog-model';
 import { TabbedLayout } from '@backstage/core-components';
@@ -66,8 +67,11 @@ class MockDqlQueryApi implements DqlQueryApi {
   async getData(): Promise<TabularData> {
     return exampleData;
   }
-  async getDataFromQueries(): Promise<TabularData[]> {
-    return [exampleData, exampleData];
+  async getDataFromQueries(): Promise<NotebookQueryData[]> {
+    return [
+      { data: exampleData, title: '' },
+      { data: exampleData, title: '' },
+    ];
   }
 }
 
@@ -75,8 +79,8 @@ class MockDqlQueryApiNoResult implements DqlQueryApi {
   async getData(): Promise<TabularData> {
     return [];
   }
-  async getDataFromQueries(): Promise<TabularData[]> {
-    return [[]];
+  async getDataFromQueries(): Promise<NotebookQueryData[]> {
+    return [];
   }
 }
 
@@ -84,7 +88,7 @@ class MockDqlQueryApiError implements DqlQueryApi {
   async getData(): Promise<TabularData> {
     throw new Error('404 Not Found');
   }
-  async getDataFromQueries(): Promise<TabularData[]> {
+  async getDataFromQueries(): Promise<NotebookQueryData[]> {
     throw new Error('404 Not Found');
   }
 }
