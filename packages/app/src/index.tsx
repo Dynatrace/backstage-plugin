22a0d5b026278fc2f1bb0371bbe7cacfa89015e9
tobias.blasge <tobias.blasge@dynatrace.com>
2025-09-16 08:16:16 +0200

    migrated react-17 to react-18

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -16,6 +16,6 @@
 import App from './App';
 import '@backstage/cli/asset-types';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
